f7a6e1f4e0c0e998817a45d18ecf0365cd3d21b5
Gracjan Litowinski <gracjan.litowinski@gmail.com>
2023-07-22 18:24:38 +0200

    front page

--- b-note-ui/src/app/app.component.ts
+++ b-note-ui/src/app/app.component.ts
@@ -1,10 +1,41 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('displaySearchApp', [
+      state('open', style({
+        position: 'absolute',
+        top: '10%',
+        left: '50%',
+        transform: 'translate(auto, -50%)',
+      })),
+      state('closed', style({
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      })),
+      transition('* => closed', [
+        animate('1s')
+      ]),
+      transition('* => open', [
+        animate('1s')
+      ])
+    ]),
+  ]
 })
 export class AppComponent {
   title = 'b-note-ui';
+
+  displaySearchApp = false;
+  searchTerm = new FormControl('', [Validators.required])
+
+  showSearchApp() {
+    this.displaySearchApp = !this.displaySearchApp;
+  }
 }
--- b-note-ui/src/app/app.module.ts
+++ b-note-ui/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -11,6 +18,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    BrowserAnimationsModule, 
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
